042581ea89a9969c7c93a0126ae10f8e8b379830
somax <somaxj@163.com>
2017-04-13 19:47:43 +0800

    全局命令

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
 # Liny
 
-使用自定义过滤器，对文本按行进行过滤清理的工具
+使用自定义过滤器，对文本按行进行处理的工具，支自动编码转换。
 
 ## Usage
-```
+
+```bash
 node . filter-demo  < data/demo.txt > output.txt
 ```
 
-## Test filter
+## Add filter
+
+在 filters 目录中添加新的过滤器 `./filters/filter-new.js`
+
+```js
+module.exports = function (line) {
+    if (/^[0-9]+,/.test(line)) {
+        console.log(line);
+    }
+}
 ```
+
+```bash
+node . filter-new  < data/demo.txt > output.txt
+```
+
+## Test filter
+
+```bash
 echo '234,abcd\n345,efg\nhijk' | node . filter-demo
 ```
--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+#!/usr/bin/env node
 const rl = require('./lib/readline');
 const filterName = process.argv[2];
-if (!filterName) throw Error('未指定过滤器，请查看 README.md');
-const filter = require(`./filters/${filterName}`);
+
+if (!filterName){
+    console.log('未指定过滤器，请查看 README.md');
+    process.exit(1);
+};
+
+
+let filter;
+try {
+    filter = require(`./filters/${filterName}`);
+} catch (error) {
+    console.log(`过滤器 "${filterName}" 未能成功加载`);
+    process.exit(1);
+}
+
+console.log(filter.toString());
 
 rl(filter);
+
