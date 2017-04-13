1079ea6d963067d6ab9af7f314b5faafff7012c3
somax <somaxj@163.com>
2017-04-13 19:47:20 +0800

    auto decode

--- filters/filter-demo.js
+++ filters/filter-demo.js
@@ -1,6 +1,5 @@
-var iconv = require('iconv-lite');
-module.exports = function(line){
-        if(/^[0-9]+,/.test(line)){
-            console.log(line); 
-        }
+module.exports = function (line) {
+    if (/^[0-9]+,/.test(line)) {
+        console.log(line);
     }
+}
